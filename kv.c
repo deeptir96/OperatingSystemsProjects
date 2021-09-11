0b0766e8dc37a5f1c246cad0a0f0befa9af62404
Deepti Rajagopal <deeptir96@royal-01.cs.wisc.edu>
2021-09-11 16:15:44 -0500

    P1a: Reads and writes into file, but has duplicates

--- kv.c
+++ kv.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
   char *inputs[argc][3];
   char value[30];
   int key;
+  FILE *fp;
 
   for(i = 1; i < argc; i++) {
     printf("Argument %d: %s\n", i, argv[i]);
@@ -21,31 +22,71 @@ int main(int argc, char *argv[]) {
 	}
     	inputs[i][k++] = inp;
     }
+    int ctr;
+    printf("Inputs\n");
+    for(ctr = 0; ctr < 3; ctr++) {
+	    printf("input %d = %s ", ctr, inputs[i][ctr]);
+    }
+    printf("\n");
     if(k > 3) {
      printf("Bad command\n");
     }
     char command = (char) inputs[i][0][0];
     switch(command) {
     	case 'p': 
-	  //do something
+	  //put/update key value pair into map
 	  key = atoi(inputs[i][1]);
+	  printf("Input [2] = %s\n", inputs[i][2]);
 	  if(inputs[i][2] == NULL) {
-		  printf("Argh bad command\n");
+		  printf("Put arghh bad command\n");
 		  break;
 	  }
 	  strcpy(value, inputs[i][2]);
 	  if(value == NULL) {
-   	    printf("Bad command\n");
+   	    printf("Put Bad command\n");
+	    break;
+	  }
+	  fp = fopen("key_value.csv", "a");
+//	  char finalString = inputs[i][1];
+//	  strcat(finalString, ",");
+//	  strcat(finalString, inputs[i][2]);
+  //        printf("Helloooofinal = %s\n", finalString);
+	  printf("Helloooo inputs[i][1] = %s\n", inputs[i][1]);
+          printf("Helloooo inputs[i][2] = %s\n", inputs[i][2]);
+	  fprintf(fp, "%s,", inputs[i][1]);
+          fprintf(fp, "%s \n", inputs[i][2]);
+	  fclose(fp);
+	  break;
+	case 'g': 
+	  //do something
+	  key = atoi(inputs[i][1]);
+	  if(inputs[i][2] != NULL) {
+	  	printf("Get Argh bad command\n");
+                break;
 	  }
 	  break;
-	case 'g': //do something
-
+	case 'c': 
+	  //do something
+	  key = atoi(inputs[i][1]);
+          if(inputs[i][1] !=NULL || inputs[i][2] != NULL) {
+                printf("Clear Argh bad command\n");
+                break;
+          }
 	  break;
-	case 'c': //do something
+ 	case 'd': 
+	  //do something
+	  key = atoi(inputs[i][1]);
+          if(inputs[i][2] != NULL) {
+                printf("DeleteArgh bad command\n");
+                break;
+          }
 	  break;
- 	case 'd': //do something
-	  break;
-	case 'a': //do something
+	case 'a': 
+	  //do something
+	  if(inputs[i][1] !=NULL || inputs[i][2] != NULL) {
+                printf("All argh bad command\n");
+                break;
+          }
 	  break;
 	default: 
 	  printf("Bad command\n");
