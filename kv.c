d1a704388c0a45598f6d3d6d779982db8645b42f
Deepti Rajagopal <deeptir96@royal-01.cs.wisc.edu>
2021-09-13 11:22:33 -0500

    P1a: Update comments

--- kv.c
+++ kv.c
@@ -17,20 +17,20 @@ int main(int argc, char *argv[]) {
   char *line = NULL;
 
   for(i = 1; i < argc; i++) {
-    printf("Argument %d: %s\n", i, argv[i]);
+//    printf("Argument %d: %s\n", i, argv[i]);
     int k=0;
     while( (inp = strsep(&argv[i], ",")) != NULL) {
         if(k == 3) {
-//		printf("Invalid number of arguments/bad command");
+		printf("Invalid number of arguments/bad command");
 		break;
 		//Handle this somehow outside as well
 	}
     	inputs[i][k++] = inp;
     }
     int ctr;
-    printf("Inputs\n");
+  //  printf("Inputs\n");
     for(ctr = 0; ctr < 3; ctr++) {
-	    printf("input %d = %s ", ctr, inputs[i][ctr]);
+//	    printf("input %d = %s ", ctr, inputs[i][ctr]);
     }
     printf("\n");
     if(k > 3) {
@@ -39,40 +39,40 @@ int main(int argc, char *argv[]) {
     char command = (char) inputs[i][0][0];
     switch(command) {
     	case 'p': 
-	  //put/update key value pair into map
+	  //Put/update key value pair into map
 	  //TODO: handle mem allocation
 	  key = atoi(inputs[i][1]);
-	  printf("Input [2] = %s\n", inputs[i][2]);
+//	  printf("Input [2] = %s\n", inputs[i][2]);
 	  if(inputs[i][2] == NULL) {
-		  printf("Put arghh bad command\n");
+		  printf("Bad command\n");
 		  break;
 	  }
 	  strcpy(value, inputs[i][2]);
 	  if(value == NULL) {
-   	    printf("Put Bad command\n");
+   	    printf("Bad command\n");
 	    break;
 	  }
 	  fpr = fopen("key_value.csv", "r");
 	  if(fpr == NULL) {
-		  printf("fpr null\n");
+//		  printf("fpr null\n");
 	    fp = fopen("key_value.csv", "a");
-	    printf("Open key_value\n");
+//	    printf("Open key_value\n");
 	    fprintf(fp, "%s,", inputs[i][1]);
             fprintf(fp, "%s \n", inputs[i][2]);
 	    fclose(fp);
 	  } else {
-            printf("fpr not null\n");
+//            printf("fpr not null\n");
    	    fp2 = fopen("temp.csv", "a");
             char currKey[30];
             while( getline(&line, &len, fpr) != -1 ) {
               strcpy(currKey, line);
-              printf("Line: %s\n", currKey);
+//              printf("Line: %s\n", currKey);
 	      char *token = strtok(currKey, ",");
-	      printf("Key %s\n", token);
-              printf("inputs[i][1] %s\n", inputs[i][1]);
-              printf("Line again: %s\n", line);
+//	      printf("Key %s\n", token);
+//              printf("inputs[i][1] %s\n", inputs[i][1]);
+//              printf("Line again: %s\n", line);
               if(atoi(token) != atoi(inputs[i][1])) {
-               printf("Current line: %s\n", line);
+//               printf("Current line: %s\n", line);
                fprintf(fp2, "%s", line);
               } else {
                 continue;
@@ -87,17 +87,17 @@ int main(int argc, char *argv[]) {
 	  }
 	  break;
 	case 'g': 
-	  //do something
+	  //Gets specific key value pair
 	  key = atoi(inputs[i][1]);
 	  if(inputs[i][2] != NULL) {
-	  	printf("Get Argh bad command\n");
+	  	printf("Bad command\n");
                 break;
 	  }
 	  break;
 	case 'c': 
-	  //do something
+	  //Clears all key value pairs stored so far
           if(inputs[i][1] !=NULL || inputs[i][2] != NULL) {
-                printf("Clear Argh bad command\n");
+                printf("Bad command\n");
                 break;
           }
 	  fp = fopen("key_value.csv", "w");
@@ -105,17 +105,17 @@ int main(int argc, char *argv[]) {
 	  fclose(fp);
 	  break;
  	case 'd': 
-	  //do something
+	  //Deletes specific key value pair
 	  key = atoi(inputs[i][1]);
           if(inputs[i][2] != NULL) {
-                printf("DeleteArgh bad command\n");
+                printf("Bad command\n");
                 break;
           }
 	  break;
 	case 'a': 
-	  //prints all the key value pairs
+	  //Prints all the key value pairs
 	  if(inputs[i][1] !=NULL || inputs[i][2] != NULL) {
-                printf("All argh bad command\n");
+                printf("Bad command\n");
                 break;
           }
 	  printf("Displaying all key value pairs:\n");
